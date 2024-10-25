5a69eb8ba3109c170a93ac64916a7df0b576c403
Tuitala <tlasifo@gmail.com>
2024-10-24 20:18:01 -1000

    update axios base url by adding back /api

--- src/http-common.jsx
+++ src/http-common.jsx
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURLs = {
+  local: "http://localhost:8080/api",
+  heroku: "https://kioa-2024-server-ccf6ffa05423.herokuapp.com/api"
+};
+
 export default axios.create({
-  baseURL: "https://kioa-2024-server-ccf6ffa05423.herokuapp.com",
+  baseURL: baseURLs.heroku, // Set the default base URL to the local URL
   headers: {
     "Content-type": "application/json"
   }
